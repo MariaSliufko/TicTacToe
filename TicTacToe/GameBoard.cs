352afc0fc93cb9ca600a0ae80be17fa77230462e
MariaSliufko <maria.sliufko@icloud.com>
2022-12-30 14:41:18 +0100

    Initial commit

---
+++ TicTacToe/GameBoard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace TicTacToe
+{
+	public class GameBoard
+	{
+        public class TicTacToen
+        {
+            private char[,] _board = new char[3, 3];
+            public char _currentPlayer = 'X';
+
+            public TicTacToen()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        _board[i, j] = ' ';
+                    }
+                }
+            }
+
+            public bool MakeMove(int row, int col)
+            {
+                if (_board[row, col] != ' ')
+                {
+                    return false;
+                }
+
+                _board[row, col] = _currentPlayer;
+                _currentPlayer = _currentPlayer == 'X' ? 'O' : 'X';
+                return true;
+            }
+
+            public char CheckWinner()
+            {
+                // Check rows
+                for (int i = 0; i < 3; i++)
+                {
+                    if (_board[i, 0] != ' ' && _board[i, 0] == _board[i, 1] && _board[i, 1] == _board[i, 2])
+                    {
+                        return _board[i, 0];
+                    }
+                }
+
+                // Check columns
+                for (int i = 0; i < 3; i++)
+                {
+                    if (_board[0, i] != ' ' && _board[0, i] == _board[1, i] && _board[1, i] == _board[2, i])
+                    {
+                        return _board[0, i];
+                    }
+                }
+
+                // Check diagonals
+                if (_board[0, 0] != ' ' && _board[0, 0] == _board[1, 1] && _board[1, 1] == _board[2, 2])
+                {
+                    return _board[0, 0];
+                }
+                if (_board[0, 2] != ' ' && _board[0, 2] == _board[1, 1] && _board[1, 1] == _board[2, 0])
+                {
+                    return _board[0, 2];
+                }
+
+                return ' ';
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        sb.Append(_board[i, j]);
+                        if (j < 2)
+                        {
+                            sb.Append(" | ");
+                        }
+                    }
+                    sb.AppendLine();
+                    if (i < 2)
+                    {
+                        sb.AppendLine("---------");
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+
+    }
+}
+
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static TicTacToe.GameBoard;
+
+Console.WriteLine("Hello, World!");
+
+//static void Main(string[] args)
+{
+    var game = new TicTacToen();
+    while (true)
+    {
+        Console.WriteLine(game);
+        Console.WriteLine($"Player {game._currentPlayer}, enter your move (row col): ");
+        var input = Console.ReadLine();
+        var parts = input.Split(' ');
+        if (parts.Length != 2)
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+            continue;
+        }
+
+        if (!int.TryParse(parts[0], out int row) || !int.TryParse(parts[1], out int col))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+            continue;
+        }
+
+        if (!game.MakeMove(row, col))
+        {
+            Console.WriteLine("Invalid move. Please try again.");
+            continue;
+        }
+
+        var winner = game.CheckWinner();
+        if (winner != ' ')
+        {
+            Console.WriteLine($"Player {winner} wins!");
+            Console.ReadLine();
+            break;
+        }
+    }
+}
